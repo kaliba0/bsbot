1287298e4eecff6118fc070fcc45a9c270f458bc
kaliba0 <143097101+kaliba0@users.noreply.github.com>
2024-08-01 19:55:39 +0200

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Rafaa',
+      script: './main.js', // Remplacez par le chemin correct vers votre script principal
+      interpreter: '/home/aterzn/.nvm/versions/node/v18.20.4/bin/node', // Remplacez par le chemin correct vers Node.js v18
+    }
+  ]
+};
